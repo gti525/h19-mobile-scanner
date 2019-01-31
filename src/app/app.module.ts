92f33fad773a4a5932a618077b079c6aced7b197
Amélie Nguyen <tuong_vi06@hotmail.com>
2019-01-31 00:39:06 -0500

    Page Etat-Connexion: Ajout de 3 slider. Manque a les remplir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { EtatConnexionPage } from '../pages/etat-connexion/etat-connexion';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
-    ParametresPage
+    ParametresPage,
+    EtatConnexionPage
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     MyApp,
     HomePage,
     ListPage,
-    ParametresPage
+    ParametresPage,
+    EtatConnexionPage
   ],
   providers: [
     StatusBar,
