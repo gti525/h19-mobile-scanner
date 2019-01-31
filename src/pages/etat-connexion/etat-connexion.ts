88a9b29d3c5d1fc08728ad271d2efa9eb0b277cd
Amélie Nguyen <tuong_vi06@hotmail.com>
2019-01-31 01:24:38 -0500

    Etat-Connexion: Squelette des pages
    Manque background, couleurs, esthétique

--- src/pages/etat-connexion/etat-connexion.ts
+++ src/pages/etat-connexion/etat-connexion.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewChild } from '@angular/core';
+import { Slides } from 'ionic-angular';
 
 /**
  * Generated class for the EtatConnexionPage page.
@@ -14,12 +16,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'etat-connexion.html',
 })
 export class EtatConnexionPage {
-
+  @ViewChild('slides') slides: Slides;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EtatConnexionPage');
+  }
+
+  next() {
+    this.slides.slideNext();
+  }
+
+  prev() {
+    this.slides.slidePrev();
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
 
 
   }
+  
   onGoToEtatConnexion(){
     this.navCtrl.push(EtatConnexionPage);
   }
