6a0f1b10b33fb961d4f8f95ad2d37755c43fb995
Monica Ayala <monica.ayala.1@etsmtl.net>
2019-02-06 17:48:34 -0500

    Add plugin for barcode and add page scanner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,18 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ScannerPage } from './../pages/scanner/scanner';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ScannerPage
   ],
   imports: [
     BrowserModule,
@@ -23,11 +26,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ScannerPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/pages/scanner/scanner.ts
@@ -0,0 +1,39 @@
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { BarcodeScanner } from "@ionic-native/barcode-scanner";
+
+/**
+ * Generated class for the ScannerPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: "page-scanner",
+  templateUrl: "scanner.html"
+})
+export class ScannerPage {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private barcodeScanner: BarcodeScanner
+  ) {}
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad ScannerPage");
+    this.scanTicket();
+  }
+
+  scanTicket() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        console.log("Barcode data", barcodeData);
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+}
