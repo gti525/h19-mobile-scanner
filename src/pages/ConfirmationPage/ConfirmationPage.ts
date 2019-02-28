dd94efb5057b198dd9648943885d9bef044282d2
Monica Ayala <monica.ayala.1@etsmtl.net>
2019-02-28 00:37:31 -0500

    fix route of the button in the ConfirmationPage

--- src/pages/ConfirmationPage/ConfirmationPage.ts
+++ src/pages/ConfirmationPage/ConfirmationPage.ts
@@ -1,20 +1,18 @@
-import { ScannerPage } from './../scanner/scanner';
-import { NavParams } from 'ionic-angular';
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ScannerPage } from "./../scanner/scanner";
+import { NavParams } from "ionic-angular";
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-    selector: 'page-ConfirmationPage',
-    templateUrl: 'ConfirmationPage.html'
+  selector: "page-ConfirmationPage",
+  templateUrl: "ConfirmationPage.html"
 })
-
-export class ConfirmationPage{
-    ticketID: any;
-    constructor(public navCtrl: NavController,  public navParams:NavParams){
-      this.ticketID = navParams.get('ticketText');
-    }
-    onGoToScanner(){
-        this.navCtrl.push(ScannerPage);
-      }
+export class ConfirmationPage {
+  ticketID: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.ticketID = navParams.get("ticketText");
+  }
+  onGoToScanner() {
+    this.navCtrl.push(ScannerPage);
+  }
 }
-
--- src/pages/nonValidePage/nonValidePage.ts
+++ src/pages/nonValidePage/nonValidePage.ts
@@ -1,21 +1,18 @@
-import { ScannerPage } from './../scanner/scanner';
-import { NavParams } from 'ionic-angular';
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ScannerPage } from "./../scanner/scanner";
+import { NavParams } from "ionic-angular";
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-    selector: 'page-nonValidePage',
-    templateUrl: 'nonValidePage.html'
+  selector: "page-nonValidePage",
+  templateUrl: "nonValidePage.html"
 })
-
-export class nonValidePage{
-    ticketID: any;
-
-    constructor(public navCtrl: NavController, public navParams:NavParams){
-      this.ticketID = navParams.get('ticketText');
-    }
-    onGoToScanner(){
-        this.navCtrl.push(ScannerPage);
-      }
+export class nonValidePage {
+  ticketID: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.ticketID = navParams.get("ticketText");
+  }
+  onGoToScanner() {
+    this.navCtrl.push(ScannerPage);
+  }
 }
-
