2dc35fe42554c98fbb39278482d566ca1bb30429
Amélie Nguyen <tuong_vi06@hotmail.com>
2019-03-14 23:22:25 -0400

    Disable generic beep, add positive/negative sound

--- src/pages/scanner/scanner.ts
+++ src/pages/scanner/scanner.ts
@@ -41,10 +41,26 @@ export class ScannerPage {
   goToValidTicket(){
     this.navCtrl.push(ConfirmationPage);
   }
+  
+  playAudio(sound){
+    let audio = new Audio();
+    if (sound = "positive"){
+      alert(sound)
+      audio.src = "/assets/sounds/positive.wav";
+    }
+    else{
+      alert(sound)
+      audio.src = "/assets/sounds/negative.wav";
+    }
+    audio.load();
+    audio.play();
+  }
+
 
   scanTicket() {
     this.options = {
-      prompt: "Placez le code barre dans la zone rectangulaire"
+      prompt: "Placez le code barre dans la zone rectangulaire",
+      disableSuccessBeep: false
     };
     this.barcodeScanner
       .scan(this.options)
@@ -53,11 +69,13 @@ export class ScannerPage {
           // TODO: the code should coe from the API
           let code = "ed36a534-3acd-11e9-b210-d663bd873d93"
           if(barcodeData.text == code ){
+              this.playAudio("positive");
               this.navCtrl.push(ConfirmationPage, {
                 ticketText: barcodeData.text
               });
           }
           else{
+            this.playAudio("negative");
             this.navCtrl.push(nonValidePage, {
               ticketText: barcodeData.text
             });
