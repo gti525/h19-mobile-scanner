e395b45ea8b138d546687431ba61105b353cee87
Amélie Nguyen <tuong_vi06@hotmail.com>
2019-03-26 17:49:24 -0400

    Nouveau pop up non connexion, il faut attendre un bon 30 sec

--- src/pages/ConnexionPage/connexion.ts
+++ src/pages/ConnexionPage/connexion.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Alert } from 'ionic-angular';
 import { ParametresPage } from '../parametres/parametres';
 import { ConfirmationPage } from '../ConfirmationPage/ConfirmationPage';
 import { EtatConnexionPage } from '../etat-connexion/etat-connexion';
 import { nonValidePage } from '../nonValidePage/nonValidePage';
 import { ScannerPage } from './../scanner/scanner';
 import { RaspiApiProvider } from "./../../providers/raspi-api/raspi-api";
+import { AlertController }  from 'ionic-angular';
 
 @Component({
     selector: 'page-connexion',
@@ -18,7 +19,7 @@ export class ConnexionPage{
     public user;
     public password;
 
-    constructor(public navCtrl: NavController, private serviceApi: RaspiApiProvider){
+    constructor(public navCtrl: NavController, private serviceApi: RaspiApiProvider, private alertController:AlertController){
       
     }
    onGoToParametre(){
@@ -40,6 +41,16 @@ export class ConnexionPage{
     onGoToScannerPage(){
       this.navCtrl.push(ScannerPage);
     }
+
+    popUpNonConnexion(){
+      let alert = this.alertController.create({
+        title: 'Oups!',
+        message: "Identifiant ou mot de passe incorrect. Tapez l'ID utilisateur et le mot de passe corrects et réessayez.",
+        buttons: ['Ok']
+      });
+      alert.present();
+
+    }
     
     login(){
       // alert("login: "+this.user+" "+"password: "+this.password);
@@ -55,7 +66,7 @@ export class ConnexionPage{
         },
         err => {
           //Gestion de la mauvaise connexion ici, mauvaise user/mdp
-          alert("Vous ne pouvez pas vous connecter");
+          this.popUpNonConnexion();
           console.log(err.status);
         }
       );
